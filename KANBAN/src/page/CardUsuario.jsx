d0d5615342a6ae41e15e25bbe69337c53f743e94
nicolasalvesss <nicolasvilelabarros@gmail.com>
2025-08-29 09:06:28 -0300

    ...

--- KANBAN/src/page/CardUsuario.jsx
+++ KANBAN/src/page/CardUsuario.jsx
@@ -1,33 +1,57 @@
-import axios from "axios";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers";
-
-// validação de formulário
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+ 
+//validação de formulário
 const schemaCadUsuario = z.object({
     nome: z.string()
-        .min(1, 'Insira ao menos 1 caractere')
+        .min(1,'Insira ao menos 1 caractere')
         .max(30, 'Insira até 30 caracteres'),
-    email: z.string()
-        .min(1, 'Insira o seu email')
-        .max(30,'Insira um endereço de email com até 30 caracteres')
-        .email("Formato de email invalido")
+    email:z.string()
+        .min(1, 'Insira seu email')
+        .max(30, 'Insira um endereço de email com até 30 carateres')
+        .email("Formato de email invalido"),
 })
+ 
+ 
+export function CadUsuario(){
 
-export function CardUsuario() {
-    return (
-        <main className="main">
-            <form>
-                <h2>Cadastro de Usuários</h2>
+    const {
+        register,
+        handleSubmit,
+        formState:{ errors },
+        reset
+    }= useForm({
+        resolver: zodResolver(schemaCadUsuario)
+    });
 
-                <label htmlFor="name">Nome:</label>
-                <input type="text" placeholder="Nome do usuário" required/>
+    //checar se estamos recebendo os dados 
+    async function obterdados(data) {
+        console.log('dados informados pelo user:', data)
+    }
 
-                <label htmlFor="email">Nome:</label>
-                <input type="email" placeholder="Email do usuário" required/>
+    try{
+        await.axios.post("http://127.0.0.1:8000/api/usuario/", data);
+        alert("Usuario cadastrado com sucesso");
+        reset();
+    }catch(errors){
+        alert("Éeee, não rolou, na proxima talvez")
+        console.log("Erros", error)
+    }
 
-                <button type="submit">Cadastrar</button>
-            </form>
-        </main>
-    );
+    return(
+        <form className="formularios">
+            <h2>Cadastro do Usuário</h2>
+ 
+            <label>Nome:</label>
+            <input type='text' placeholder='Jose da Silva' required/>
+ 
+            <label>E-mail</label>
+            <input type='email' placeholder='email@email.com' required/>
+ 
+            <button type='submit'>Cadastrar</button>
+ 
+        </form>
+    )
 }
